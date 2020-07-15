d920240ae85595b8efd9e6bc185ac359c395b3e3
jannabel <jannabelramos@hotmail.com>
2020-07-15 14:36:44 -0400

    Proyecto

---
+++ Atentificacion y Seguridad/Autentificar.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+
+public class Autentificar
+{
+
+     public static bool comprobar(long[] usuarios, string[] rol, bool[] estado, int[] password, string clave, long user, DateTime[] fecha_de_creacion)
+      {
+
+        int clav=0;
+        Int32.TryParse(clave, out clav);
+        int cont = 0;
+
+        for (int i = 0; i < usuarios.Length; i++)
+        {
+            
+            if (usuarios[i] == user && password[i] == clav && estado[i] == true)
+            {
+                cont++;                
+            }
+
+            if (i == usuarios.Length-1 && cont == 0)
+            {
+                Console.WriteLine("\n\n ~ Usuario y/o clave incorrectos ~ ");
+                Console.WriteLine();
+                return false;
+            }
+            else if (cont == 1)
+            {
+                Console.WriteLine("\n\n ~ Acabas de ingresar con el usuario {0} y su rol es {1}", usuarios[i], rol[i]);
+                Console.WriteLine(" ~ Este usuario fue creado : {0}", fecha_de_creacion[i]);
+                Console.WriteLine();
+                return true;
+            }
+
+            if (usuarios[i] == user && password[i] == clav && estado[i] == false)
+            {
+                Console.WriteLine("\n\n  ~ Este usuario esta inactivo ~ ");
+                Console.WriteLine();
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
---
+++ Atentificacion y Seguridad/Program.cs
@@ -0,0 +1,109 @@
+﻿//Jannabel Ramos Ramírez 2019-8510
+
+/*En el siguiente proyecto crearemos una aplicación que pida al usuario su (username y clave). 
+    El usuario sera el No. de cedula y la clave debe ser numerica, el sistma no debe permitir que
+    el usuario acceda hasta que ponga su autenticacion correcta.
+
+    El sistema debe tener al menos 3 usuarios registrados para validar, y los usuarios 
+    al acceder tendran un rol (Supervisor, Administrador, Vendedor). 
+    De acuerdo a su rol le mostrara el mensaje al acceder 
+    "Acabas de ingresar con el usuario XXXX y su rol es XXXXX."
+
+    Los usuarios algunos tendran un estado de (activo o inactivo) en caso de inactivo 
+    sea positivo el usuario no podra acceder, el sistema debe mostrar un mensaje que ese
+    usuario esta inactivo y volver a validar otro usuario.
+
+    Datos de usuarios: Nombre, username, clave, rol, fecha de creacion, estado (Activo o inactivo).*/
+
+using System;
+
+namespace Atentificacion_y_Seguridad
+{
+    class Program
+    {
+               
+        static void Main(string[] args)
+        {
+            long[] usuarios = { 00102852605, 12345678910, 25689741256, 46100000451};
+            int[] password = { 1234, 5670, 8901, 1010 };
+            String[] rol = { "Supervisor", "Administrador", "Vendedor" , "Vendedor" };
+            bool[] estado= { true, true, false, true};
+
+             DateTime fecha1 = new DateTime(2020, 7, 14); DateTime fecha2 = new DateTime(2020, 7, 13);
+             DateTime fecha3 = new DateTime(2020, 6, 10);  DateTime fecha4 = new DateTime(2020, 1, 11);
+
+             DateTime[] fecha_de_creacion = {fecha1, fecha3, fecha3, fecha4};
+
+             Console.WriteLine("\n .............................................");
+             Console.WriteLine(" ............. SISTEMA DE ACCESO ............. ");
+             Console.WriteLine(" .............................................");
+
+             Console.Write("\n ~ Ingrese su usuario: ");
+             long user = Convert.ToInt64(Console.ReadLine());
+             Console.Write(" ~ Ingrese su clave: ");
+
+            string pass = "";
+
+            do
+            {
+                ConsoleKeyInfo key = Console.ReadKey(true);
+
+                if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                {
+                pass += key.KeyChar;
+                Console.Write("*");
+                }
+                else
+                {
+                    if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+                    {
+                    pass = pass.Substring(0, (pass.Length - 1));
+                    Console.Write("\b \b");
+                    }
+                    else if (key.Key == ConsoleKey.Enter)
+                    {
+                        break;
+                    }
+                }
+            } while (true);
+     
+            while (Autentificar.comprobar(usuarios, rol, estado, password, pass, user, fecha_de_creacion) == false)
+             {
+             pass = "";
+             Console.ReadKey();
+             Console.Clear();
+             Console.WriteLine("\n .............................................");
+             Console.WriteLine(" ............. SISTEMA DE ACCESO ............. ");
+             Console.WriteLine(" .............................................");
+             Console.Write("\n ~ Ingrese su usuario: ");
+             user = Convert.ToInt64(Console.ReadLine());
+             Console.Write(" ~ Ingrese su clave: ");
+
+             do
+                 {
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    if (key.Key != ConsoleKey.Backspace && key.Key != ConsoleKey.Enter)
+                    {
+                        pass += key.KeyChar;
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        if (key.Key == ConsoleKey.Backspace && pass.Length > 0)
+                        {
+                            pass = pass.Substring(0, (pass.Length - 1));
+                            Console.Write("\b \b");
+                        }
+                        else if (key.Key == ConsoleKey.Enter)
+                        {
+                            break;
+                        }
+                    }
+                
+             } while (true);
+        }
+                        
+
+        }
+    }
+}
